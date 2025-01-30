dee6a598aa909924744e9d91c41ecfd4a1058ad6
Aliaksandr Bykouski <Aliaksandr Bykouski>
2025-01-30 13:06:18 +0100

    add section slider and 1/3 section catalog

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+let index = 0;
+
+function moveSlide(step) {
+  const slides = document.querySelectorAll(".slide");
+  const totalSlides = slides.length;
+
+  index += step;
+
+  if (index < 0) {
+    index = totalSlides - 1;
+  } else if (index >= totalSlides) {
+    index = 0;
+  }
+
+  const carousel = document.querySelector(".slides");
+  carousel.style.transform = `translateX(-${index * 100}%)`; // сдвиг на ширину одного слайда
+}
