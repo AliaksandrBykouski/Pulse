e38c4edf552cbef196e309bef6398f4ca2f6f9ef
Aliaksandr Bykouski <Aliaksandr Bykouski>
2025-02-01 00:32:03 +0100

    add section feedback

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,15 @@ function moveSlide(step) {
   const carousel = document.querySelector(".slides");
   carousel.style.transform = `translateX(-${index * 100}%)`; // сдвиг на ширину одного слайда
 }
+function flipCard(card) {
+  card.classList.toggle("flipped");
+}
+
+function showTab(index) {
+  document.querySelectorAll(".catalog-button").forEach((tab, i) => {
+    tab.classList.toggle("active", i === index);
+  });
+  document.querySelectorAll(".catalog__card").forEach((container, i) => {
+    container.classList.toggle("active", i === index);
+  });
+}
